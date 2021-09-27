d70702b476fac7b3841becd5fa6c04d867d8b085
eleftheros <eleftheros_all@protonmail.com>
2021-09-27 20:30:21 +0300

    Add getRecipes and saveRecipe methods in LocalStorage

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -2,8 +2,14 @@ class LocalStorage {
   constructor() {
 
   }
+  getRecipes = () => {
+    return JSON.parse(localStorage.getItem("recipes")) || [];
+  }
   saveRecipe(id) {
+    const recipes = this.getRecipes();
 
+    recipes.push({id});
+    localStorage.setItem("recipes", JSON.stringify(recipes));
   }
 }
 
