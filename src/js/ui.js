c6aa5f7531417a7a9b1c5ad57563fb0d09606afe
eleftheros <eleftheros_all@protonmail.com>
2021-09-27 12:26:19 +0300

    Add toggleForm method in UI

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,16 +1,21 @@
 class UI {
   constructor() {
+    this.searchForm = document.getElementById("searchForm");
     this.searchInput = document.getElementById("searchInput");
   }
   init() {
-    this.searchInput.addEventListener("focus", e => {
+    this.searchInput.addEventListener("focus", this.toggleForm);
+    this.searchInput.addEventListener("blur", this.toggleForm);
+  }
+  toggleForm(e) {
+    if (e.type === "focus") {
       e.currentTarget.parentElement.classList.add("active");
       e.currentTarget.placeholder = "";
-    });
-    this.searchInput.addEventListener("blur", e => {
+    } 
+    else if (e.type === "blur") {
       e.currentTarget.parentElement.classList.remove("active");
       e.currentTarget.placeholder = "Search for a recipe...";
-    });
+    }
   }
 }
 
